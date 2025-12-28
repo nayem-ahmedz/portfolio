9c96fbb2311defe9b494d391ed2968c6f096d170
nayem-ahmedz <nayemahmed2n@gmail.com>
2025-12-28 14:45:38 +0600

    Image empty container with colors

--- src/components/shared/ProfileImage.tsx
+++ src/components/shared/ProfileImage.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function ProfileImage(){
     return(
-        <figure className="w-xs md:w-sm bg-gray-50 aspect-square rounded-full">
+        <figure className="w-xs md:w-sm bg-gray-200 aspect-square rounded-full">
             <Image src='/media/images/nayem-ahmed.webp' alt='Nayem Ahmed' width={500} height={500} loading="eager" className="w-full rounded-full shadow-2xl" />
         </figure>
     );
